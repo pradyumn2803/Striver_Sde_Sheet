c2306821e47008e4c7471d9ee31a723b90446ba9
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-04 22:31:31 +0530

    added matrix_0

---
+++ Day 1/set_matrix_zero.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+void setZeros(vector<vector<int>> &matrix)
+{
+	int n= matrix.size();
+    int m=matrix[0].size();
+    int col=1;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(matrix[i][0]==0) col=0;
+        for(int j=1;j<m;j++)
+        {
+            if(matrix[i][j]==0)
+            {
+                matrix[i][0]=0;
+                matrix[0][j]=0;
+            }
+        }
+    }
+    
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=m-1;j>0;j--)
+        {
+            if(matrix[i][0]==0 || matrix[0][j]==0)
+            {
+                matrix[i][j]=0;
+            }
+        }
+        if(col==0) matrix[i][0]=0;
+    } 
+}
