03d470ed3e07a8dc376d12d372704d46a1cf301a
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-06 20:55:01 +0530

    merge sorted array

---
+++ Day 2/merge_sorted_array.cpp
@@ -0,0 +1,44 @@
+int next_gap(int n)
+{
+    if(n<=1) return 0;
+    return (n/2)+(n%2);
+}
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	int i=0,j=0;
+    for(int gap=next_gap(gap);gap>0;gap=next_gap(gap))
+    {
+        for(i=0;i+gap<m;i++)
+        {
+            if(arr1[i]>arr1[i+gap])
+            {
+                swap(arr1[i],arr1[i+gap]);
+            }
+        }
+        
+        for(j=(gap>m)?(gap-m):0; i<m && j<n;i++,j++)
+        {
+            if(arr1[i]>arr2[j])
+            {
+                swap(arr1[i],arr2[j]);
+            }
+        }
+        
+        if(j<n)
+        {
+            for(j=0;j+gap<n;j++)
+            {
+                if(arr2[j]>arr2[j+gap])
+                {
+                    swap(arr2[j],arr2[j+gap]);
+                }
+            }
+        }
+    }
+    int k=0;
+    for(i=m;i<(m+n);i++)
+    {
+        arr1[i]=arr2[k++];
+    }
+    return arr1;
+}
