0ad3cd475c9f8d9e5055d4f557cacfca47e02c76
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-06 22:52:12 +0530

    count inversions

--- Day 2/missing_repeating_number.cpp
+++ Day 2/missing_repeating_number.cpp
@@ -1,3 +1,55 @@
+//Approach 1
+
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+    pair<int,int> p;
+	long long int s=(n*1ll*(n+1))/2ll;
+    long long int s2=(n*1ll*(n+1)*1ll*(2ll*n+1))/6ll;
+    
+    long long int a=0;
+    long long int b=0;
+    
+    for(int i=0;i<n;i++)
+    {
+        a+=arr[i];
+        b+=(arr[i]*1ll*arr[i]);
+    }
+	
+    s=s-a; //x-y
+    s2=s2-b; //x^2-y^2
+    s2=s2/s;
+    
+    int x=((s+s2)/2ll);
+    int y=((s2-s)/2ll);
+    
+    bool is_missing_x =true;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            is_missing_x=false;
+            break;
+        }
+    }
+    
+    if(is_missing_x==true)
+    {
+        p.first=x;
+        p.second=y;
+    }
+    else
+    {
+        p.first=y;
+        p.second=x;
+    }
+    
+    return p;
+}
+
+
+//Approach 2
 int set_bit(int n)
 {
     int ind;
