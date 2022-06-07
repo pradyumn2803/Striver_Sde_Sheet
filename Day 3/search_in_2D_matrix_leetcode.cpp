bed22d2ea0185bf7c51a47f5c1ef8891a400df0d
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-07 12:51:56 +0530

    search in 2d matrix

--- Day 3/search_in_2D_matrix_leetcode.cpp
+++ Day 3/search_in_2D_matrix_leetcode.cpp
@@ -1,25 +1,3 @@
-// Approach 1
-
-#include <vector>
-
-bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
-    if(mat.size()==0) return false;
-	
-    int i=0,j=n-1;
-    
-    while(i<m && j>=0)
-    {
-        if(mat[i][j]==target) return true;
-        
-        if(target<mat[i][j]) j--;
-        else i++;
-    }
-    
-    return false;
-}
-
-// Approach 2
-
 #include <vector>
 
 bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
