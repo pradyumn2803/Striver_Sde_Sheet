71bafb9fa096b7909ba770dcd4f0479a7e61adc4
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-08 13:42:33 +0530

    count subarrays with given xor

---
+++ Day 4/count_subarrays_with_given_xor.cpp
@@ -0,0 +1,19 @@
+#include<unordered_map>
+int subarraysXor(vector<int> &arr, int x)
+{
+    unordered_map<int,int> mp;
+    int n=arr.size();
+    int ans=0,cnt=0;
+    for(int i=0;i<n;i++)
+    {
+        ans^=arr[i];
+        
+        if(ans==x) cnt++;
+         if(mp.find(ans^x)!=mp.end())
+         {
+            cnt+=mp[ans^x];
+         }
+        mp[ans]+=1;
+    }
+    return cnt;
+}
