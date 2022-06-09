15421f1745dfaeb5150ca3262ea5083cf7ddbba8
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-09 18:20:46 +0530

    delete node in a linkedlist

---
+++ Day 5/delete_node_linkedlist_(no_head _reference).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    if(node->next==NULL) node=NULL;
+    else
+    {
+        node->data=node->next->data;
+        node->next=node->next->next;
+    }
+}
