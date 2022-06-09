e02b96157a7aafa0e234a4e6b76e91ca588db5dc
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-09 11:46:06 +0530

    longest substring without repeating characters

--- Day 4/longest_substring_without_repeating_characters.cpp
+++ Day 4/longest_substring_without_repeating_characters.cpp
@@ -1,3 +1,29 @@
+//Approach 1
+
+#include<set>
+int uniqueSubstrings(string input)
+{
+    int n=input.size();
+    int left=0,right=0;
+    int len=0;
+    set<char> s;
+    while(right<n)
+    {
+        while(left < right && s.find(input[right])!=s.end()) 
+        {
+            s.erase(input[left]);
+            left++;
+            
+        }
+        s.insert(input[right]);
+        len=max(len,right-left+1);
+        right++;
+
+    }
+    return len;
+}
+
+//Approach 2
 #include<unordered_map>
 int uniqueSubstrings(string input)
 {
