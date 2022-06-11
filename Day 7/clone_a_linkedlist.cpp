994a3fe3129b3a3c30cbd60f3bc3322815e6957f
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-11 13:05:49 +0530

    clone a linkedlist

---
+++ Day 7/clone_a_linkedlist.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the LinkedListNode class structure
+
+    template <typename T>   
+    class LinkedListNode
+    {
+        public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode<T> *random;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*************************************************************/
+
+LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
+{
+    if(head==NULL) return head;
+    LinkedListNode<int>* temp=head;
+    
+    while(temp!=NULL)
+    {
+        LinkedListNode<int>* node=new LinkedListNode<int>(temp->data);
+        node->next=temp->next;
+        temp->next=node;
+        temp=temp->next->next;
+    }
+    
+    temp=head;
+    while(temp!=NULL)
+    {
+        if(temp->random!=NULL)
+        {
+            temp->next->random=temp->random->next;
+        }
+        else temp->next->random=NULL;
+
+        temp=temp->next->next;
+    }
+    temp=head;
+    LinkedListNode<int>* d=new LinkedListNode<int>(0);
+    d->next=temp->next;
+    LinkedListNode<int>* t=d;
+    
+    LinkedListNode<int>* iter=head;
+    LinkedListNode<int>* front=head;
+    
+    while(iter!=NULL)
+    {
+        front=iter->next->next;
+        t->next=iter->next;
+        iter->next=front;
+        t=t->next;
+        iter=front;
+    }
+    
+    return d->next;
+}
