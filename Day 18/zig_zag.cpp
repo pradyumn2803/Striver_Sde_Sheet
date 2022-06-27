2381fbcad1d0fb3f213cdeeb1edb7c6823e939c7
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-06-27 12:31:07 +0530

    zig zag

--- Day 18/zig_zag.cpp
+++ Day 18/zig_zag.cpp
@@ -29,25 +29,20 @@ vector<int> zigZagTraversal(BinaryTreeNode<int> *root)
     while(!q.empty())
     {
         int sz=q.size();
-        int ans_size=ans.size();
-        ans.resize(ans_size+sz);
         
         for(int i=0;i<sz;i++)
         {
             BinaryTreeNode<int> *node=q.front();
             q.pop();
             
-            int index=(LeftToRight)?i:(sz-i-1);
-            ans[ans_size+index]=node->data;
+            if(LeftToRight==1) ans.push_back(node->data);
+            else ans.insert(ans.end()-i,node->data);
             
             if(node->left!=NULL)
-            {
                 q.push(node->left);
-            }
+            
             if(node->right!=NULL)
-            {
                 q.push(node->right);
-            }
         }
         LeftToRight=!LeftToRight;
     }
