e28c51aecfa839a9c5d92a6a513d3f185cad4ca2
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-07-01 10:58:21 +0530

    ceil

---
+++ Day 21/ceil_BST.cpp
@@ -0,0 +1,45 @@
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+
+int findCeil(BinaryTreeNode<int> *node, int x){
+    int ceil=-1;
+    
+    while(node)
+    {
+        if(node->data<x)
+        {
+            node=node->right;
+        }
+        else
+        {
+            ceil=node->data;
+            node=node->left;
+        }
+    }
+    return ceil;
+}
