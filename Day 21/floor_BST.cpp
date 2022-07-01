abbcd3e3935134251118722e5816423d841ef3dc
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-07-01 10:53:05 +0530

    floor

---
+++ Day 21/floor_BST.cpp
@@ -0,0 +1,39 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int floorInBST(TreeNode<int> * root, int x)
+{
+    if(root==NULL) return 0;
+    int floor=0;
+    
+    while(root)
+    {
+        if(x<root->val) 
+        {
+            root=root->left;
+        }
+        else
+        {
+            floor=root->val;
+            root=root->right;
+        }
+    }
+    return floor;
+}
