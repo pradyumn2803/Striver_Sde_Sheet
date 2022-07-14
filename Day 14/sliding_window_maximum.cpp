054badcd42040f849bb4f030d86b13221fa42a13
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-07-14 17:24:57 +0530

    sliding window maximum

---
+++ Day 14/sliding_window_maximum.cpp
@@ -0,0 +1,16 @@
+#include<deque>
+vector<int> slidingWindowMaximum(vector<int> &nums, int &k)
+{
+    vector<int> ans;
+    deque<int> dq;
+    for(int i=0;i<nums.size();i++)
+    {
+        while(!dq.empty() && dq.front()==(i-k)) dq.pop_front();
+        
+        while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
+        
+        dq.push_back(i);
+        if(i>=k-1) ans.push_back(nums[dq.front()]);
+    }
+    return ans;
+}
