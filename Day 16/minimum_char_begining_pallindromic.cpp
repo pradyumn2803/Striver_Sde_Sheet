c10c48fc885d6676917ba10d1c257e22772ce286
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-08-03 18:44:30 +0530

    min char begining pallindrome

--- Day 16/minimum_char_begining_pallindromic.cpp
+++ Day 16/minimum_char_begining_pallindromic.cpp
@@ -1,26 +1,22 @@
-int minCharsforPalindrome(string str) {
-    
-    int n = str.size();
-    int left=0,right=n-1;
-    int ans=0;
-    while(left<right)
+#include<algorithm>
+vector<int> find_lps(string str)
+{
+    vector<int> lps(str.size()+1,0);
+    int j=0;
+    for(int i=1;i<str.size();i++)
     {
-        if(str[left]==str[right])
-        {
-            left++;
-            right--;
-        }
-        else
-        {
-            left=0;
-            ans=n-right;
-            while(str[left]==str[right])
-            {
-                ans--;
-                left++;
-            }
-            right--;
-        }
+        while(j>0 && str[i]!=str[j]) j=lps[j-1];
+        if(str[i]==str[j]) j++;
+        lps[i]=j;
     }
-    return ans;
+    return lps;
+}
+
+int minCharsforPalindrome(string str) {
+	// Write your code here.
+	string rev=str;
+    reverse(rev.begin(),rev.end());
+    string new_str=str+'$'+rev;
+    vector<int> lps=find_lps(new_str);
+    return str.size()-lps[new_str.size()-1];
 }
