88fb51403c1b3656251f2682ebf644c4b9fc3b80
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2023-01-10 18:46:39 +0530

    modified day2 merge sorted array

--- Day 02/merge_sorted_array.cpp
+++ Day 02/merge_sorted_array.cpp
@@ -1,78 +1,83 @@
 // Approach 1
 // insertion sort method
 
-vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
-	// Write your code here.
-	int i=0;
-	if(m==0) return arr2;
-	if(n!=0)
-	{
-		while(i<m)
-		{
-			if(arr1[i]>arr2[0])
-			{
-				swap(arr1[i],arr2[0]);
-				int j=1;
-                while(j<n){
-                    if(arr2[j]<arr2[j-1]) swap(arr2[j],arr2[j-1]);
+vector<int> ninjaAndSortedArrays(vector<int> &arr1, vector<int> &arr2, int m, int n)
+{
+    // Write your code here.
+    int i = 0;
+    if (m == 0)
+        return arr2;
+    if (n != 0)
+    {
+        while (i < m)
+        {
+            if (arr1[i] > arr2[0])
+            {
+                swap(arr1[i], arr2[0]);
+                int j = 1;
+                while (j < n)
+                {
+                    if (arr2[j] < arr2[j - 1])
+                        swap(arr2[j], arr2[j - 1]);
                     j++;
                 }
-			}
-			i++;
-		}
-		
-	}
-	int j=0;
-	while(i<m+n)
-	{
-		arr1[i++]=arr2[j++];
-	}
-	
-	return arr1;
+            }
+            i++;
+        }
+    }
+    int j = 0;
+    while (i < m + n)
+    {
+        arr1[i++] = arr2[j++];
+    }
+
+    return arr1;
 }
 
-//Approach 2 Gap Method
+// Approach 2 Gap Method
 int next_gap(int n)
 {
-    if(n<=1) return 0;
-    return (n/2)+(n%2);
+    if (n <= 1)
+        return 0;
+    return (n / 2) + (n % 2);
 }
 
-vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
-	int i=0,j=0;
-    for(int gap=next_gap(gap);gap>0;gap=next_gap(gap))
+vector<int> ninjaAndSortedArrays(vector<int> &arr1, vector<int> &arr2, int m, int n)
+{
+    int i = 0, j = 0;
+    for (int gap = next_gap(gap); gap > 0; gap = next_gap(gap))
     {
-        for(i=0;i+gap<m;i++)
+        for (i = 0; i + gap < m; i++)
         {
-            if(arr1[i]>arr1[i+gap])
+            if (arr1[i] > arr1[i + gap])
             {
-                swap(arr1[i],arr1[i+gap]);
+                swap(arr1[i], arr1[i + gap]);
             }
         }
-        
-        for(j=(gap>m)?(gap-m):0; i<m && j<n;i++,j++)
+
+        for (j = (gap > m) ? (gap - m) : 0; i < m && j < n; i++, j++)
         {
-            if(arr1[i]>arr2[j])
+            if (arr1[i] > arr2[j])
             {
-                swap(arr1[i],arr2[j]);
+                swap(arr1[i], arr2[j]);
             }
         }
-        
-        if(j<n)
+
+        if (j < n)
         {
-            for(j=0;j+gap<n;j++)
+            for (j = 0; j + gap < n; j++)
             {
-                if(arr2[j]>arr2[j+gap])
+                if (arr2[j] > arr2[j + gap])
                 {
-                    swap(arr2[j],arr2[j+gap]);
+                    swap(arr2[j], arr2[j + gap]);
                 }
             }
         }
     }
-    int k=0;
-    for(i=m;i<(m+n);i++)
+    int k = 0;
+    for (i = m; i < (m + n); i++)
     {
-        arr1[i]=arr2[k++];
+        arr1[i] = arr2[k++];
     }
     return arr1;
 }
